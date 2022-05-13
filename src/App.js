c39569b74895c38d6894210f1a30d1fb510943f7
Stopalov12 <stopalov111@gmail.com>
2022-05-13 10:55:58 +0300

    Additional changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import * as React from "react";
 import { ethers } from "ethers";
-import './App.css';
+import "./App.css";
 
 export default function App() {
+  const wave = () => {};
 
-  const wave = () => {
-    
-  }
-  
   return (
     <div className="mainContainer">
-
       <div className="dataContainer">
-        <div className="header">
-        👋 Hey there!
-        </div>
+        <div className="header">👋 Hey there!</div>
 
-        <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
-        </div>
+        <div className="bio">Welcome to our community</div>
 
         <button className="waveButton" onClick={wave}>
-          Wave at Me
+          Wave at Us
         </button>
       </div>
     </div>
